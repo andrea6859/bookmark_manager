9145f15c98c954fb629663ea7bd36a6d96eacf72
Andrea Wildsmith <awildsmi@hotmail.com>
2016-09-06 11:22:14 +0100

    added database

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ require 'sinatra/base'
 class Bookmark_manager < Sinatra::Base
 
 
-  get '/' do
-    erb :index
-  end
-
-  post '/links' do
-    
-    redirect '/listed_links'
-  end
-
-  get '/listed_links' do
-    @links.list
-    erb :listed_links
-  end
+  # get '/' do
+  #   erb :index
+  # end
+  #
+  # post '/links' do
+  #
+  #   redirect '/listed_links'
+  # end
+  #
+  # get '/listed_links' do
+  #   @links.list
+  #   erb :listed_links
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
