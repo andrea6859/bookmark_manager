d3d35fe78780285a4baedd0e73df8cdae530d3df
johnnydee8 <j.dworzynski@me.com>
2016-09-06 12:05:45 +0100

    passing test for added links

--- spec/features/listing_spec.rb
+++ spec/features/listing_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 feature 'List links' do
   scenario 'User can list links' do
-    visit '/'
+
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-    visit 'listed_links'
+
+    visit '/links'
+
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-    expect(page).to have_content('Makers Academy')
+      expect(page).to have_content('Makers Academy')
     end
-  end  
+
+  end
 end
