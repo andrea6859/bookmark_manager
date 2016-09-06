96602a286529d0a42186b44e1043a896cc15221f
Andrea Wildsmith <awildsmi@hotmail.com>
2016-09-06 13:03:04 +0100

    creating links

--- app/app.rb
+++ app/app.rb
@@ -8,5 +8,15 @@ class Bookmark_manager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+   Link.create(url: params[:Website_Address], title: params[:Website_Title])
+   redirect '/links'
+  end
+
+
 run! if app_file == $0
 end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'create links' do
+  scenario 'add a site address and title' do
+    visit '/links/new'
+    fill_in('website_address', with: 'www.bbc.co.uk')
+    fill_in('website_title', with: 'BBC')
+    click_button('Add')
+    expect(page).to have_content('BBC')
+  end
+end
